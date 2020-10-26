b601466e41ebbac28bbe97c2eac95fc6fe3a4c65
JaewonP <protectingjw@naver.com>
2020-10-26 12:19:13 +0900

    commit

---
+++ springbasic1026/src/main/java/com/green/controller/Korea.java
@@ -0,0 +1,22 @@
+package com.green.controller;
+
+public class Korea {
+
+	
+	private Truck d;
+	private Calculator b;
+	private int z;
+	
+	public void setTruck (Truck d) {
+		this.d = d;
+	}
+	
+	public void setCalculator(Calculator b) {
+		this.b =b;
+	}
+	
+	public void setZ(int z) {
+		this.z = z;
+	}
+	
+}
---
+++ springbasic1026/src/main/java/com/green/controller/KoreaTest.java
@@ -0,0 +1,15 @@
+package com.green.controller;
+
+public class KoreaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Korea korea = new Korea();
+		Calculator calc = new Calculator();
+		korea.setZ(7);
+		korea.setCalculator(calc);
+		korea.setTruck(new Truck());
+
+	}
+
+}
